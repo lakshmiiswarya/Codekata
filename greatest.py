c9975f6ed9d505788ba8d26fa11f20916d4ce013
lakshmiiswarya <lakshmimurugan96@gmail.com>
2017-06-21 12:00:34 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number")
+b=int(input("enter the second number")
+c=int(input("ener the third number")
+if(a>b):
+  print "first number is greater"
+elif(b>c)and(b>a):
+  print"second number is greater"
+else:
+  print"third number is greater"
+  
