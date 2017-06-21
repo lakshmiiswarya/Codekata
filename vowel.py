f942375d67baa7169028ebebaca966460b4214e0
lakshmiiswarya <lakshmimurugan96@gmail.com>
2017-06-21 11:26:53 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,13 @@
+v=input("enter the character")
+if(v=='a'):
+  print"vowel"
+elif(v=='e'):
+  print"vowel"
+elif(v=='i'):
+  print"vowel"
+elif(v=='o'):
+  print"vowel"
+elif(v=='u'):
+  print"vowel"
+else:
+  print"consonant"
