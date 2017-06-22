1411bfa3e68a0a809fa5cbc97df2ad88d26e9214
lakshmiiswarya <lakshmimurugan96@gmail.com>
2017-06-22 10:23:36 +0530

    Create intervalprime.py

---
+++ intervalprime.py
@@ -0,0 +1,26 @@
+num = int(input("Enter a  starting number: "))
+num1=int(input("enter the ending number: "))
+
+for j in range(num,num1):
+	if j > 1:
+      
+		for i in range(2,j):
+       
+			if (j % i) == 0:
+           
+				print(j,"is not a prime number")
+           
+				print(i,"times",j//i,"is",j)
+           
+				break
+   
+		else:
+       
+			print(j,"is a prime number")
+       
+
+
+
+	else:
+   
+		print(j,"is not a prime number")
