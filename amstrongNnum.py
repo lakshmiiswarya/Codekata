26112f9f0e07f35c495b7319eaf917f9f985e493
lakshmiiswarya <lakshmimurugan96@gmail.com>
2017-06-22 11:27:28 +0530

    Create amstrongNnum.py

---
+++ amstrongNnum.py
@@ -0,0 +1,17 @@
+a=int(input("enter the starting number"))
+c=int(input("enter the ending number"))
+for j in range(a,c)
+ temp=j
+ a1=str(j)
+ b=len(a1)
+ print(b)
+ sum=0
+  while temp!=0:
+   digit = temp % 10
+   q=temp//10
+   temp=q
+   sum += digit ** b
+ if j == sum:
+   print(j,"is an Armstrong number")
+ else:
+   print(j,"is not an Armstrong number")
